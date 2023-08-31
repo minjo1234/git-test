abf7fdcd1aaaa704396f1f314172d9b6864bfaca
minjo1234 <whals777530@naver.com>
2023-08-31 12:33:10 +0900

    나 공부완료

---
+++ 07mean.py
@@ -0,0 +1,20 @@
+# 07mean.py
+import numpy as np
+
+jumsu = [175, 177, 179, 181, 183]
+
+time = [1, 2, 3, 4, 5]  # 공부한시간
+jumsu = [175, 177, 179, 181, 183]  # 점수
+
+print()
+# AI적용다양 고객응대,집값예상,주식예샹,지구온난화 예상
+
+average = 0
+hap = 0
+
+for i in range(len(jumsu)):
+    average += int(jumsu[i]) / int(len(jumsu))
+    hap += (jumsu[i])
+
+print(average)
+print(hap)
