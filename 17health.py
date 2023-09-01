c46eb1ae8e2a448a3e0fa6a4ac1a2c1ccc4375b6
minjo1234 <whals777530@naver.com>
2023-09-01 15:00:45 +0900

    15:00 githubtest.py 작성

--- 17health.py
+++ 17health.py
@@ -4,12 +4,12 @@ import time
 
 fname = './data/health.csv'
 health = pd.read_csv(fname, encoding='cp949')
-print(health.head())
+# print(health.head())
 # 위의 rownum에서 5개를 출력해준다
 print()
-print(health.tail())
+# print(health.tail())
 # 아래의 rownum에서 5개를 출력해준다 .
-
+print(health)
 print()
 print('-'*70)
 
@@ -20,3 +20,15 @@ print('결측값null 갯수', health.isnull())
 print('결측값duplicated', health.duplicated().sum())
 # 결측값이 존재하지않으므로 오류발생
 print(health.shape)
+print(health)
+print(health.tail())  # 하위 행번호 1162
+data = health[health['WEIGHT'] == 60].index
+print('결과값 = ', data)
+print(data)
+
+remove = health = health.drop(data)
+health.reset_index(drop=True, inplace=True)
+print(health)
+health.dropna(inplace=True)
+print(health)
+# remove = health.drop(data)
