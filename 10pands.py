896957cd5a593e71304e7788939ad3995223d473
minjo1234 <whals777530@naver.com>
2023-09-01 12:33:08 +0900

    12:3수정

--- 10pands.py
+++ 10pands.py
@@ -18,3 +18,12 @@ time.sleep(0.5)
 print(emp.loc[2:5])
 print(emp.iloc[2:5])
 # 2행부터 5열까지 출력하겠다
+
+print(emp.loc[2:, ['Name', 'No', 'pay']])
+
+time.sleep(0.5)
+print(emp.loc[2:5, ['Name', 'No', 'pay']])
+print()
+print(emp.iloc[2:5])  # 5행전까지만 출력된다
+
+print(emp.iloc[2:5, [1, 0, 2]])  # ilo[시작:5행-1] , [행번호숫자로명기]
