ce0a9be297433644f62cb870155124369a88ccd4
minjo1234 <whals777530@naver.com>
2023-09-01 09:27:00 +0900

    0901-1

---
+++ 01test.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+x = [5 ,7 ,3 ]
+y = [2 , 9 ,4]
+
+# 실행중에러발생 print(x,y) 리스트 
+
+print(y*5)
+print('09-01-금요일')
+print()
+
+a = np.array(x)
+b = np.array(y)
+print(a*b)
+print()
+print(b*5)
+print()
+
+print()
+print('')
+print(np.ones([3,5]))
+a
+print(np.zeros((3,5)))
+print()
+
+data = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+print(data)
---
+++ 02test.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+print()
+
+a = np.array([1, 2, 3, 4, 5 , 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17])
+
+print(np.zeros([3,5]))
+
+print(np.ones([3,5]))
+
+
+print('-'*50)
+# 5행 20열로 0부터 99까지 발생시킴 
+x = np.arange(100).reshape(5,20)
+print(x)
+np.random.shuffle(x)
+# 섞음 
+print(x)
